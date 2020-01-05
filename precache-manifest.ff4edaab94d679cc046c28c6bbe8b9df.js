3ea6f317de05011ff0780ff65bb68a3620e66287
Mirza Setiyono <mirzasty@gmail.com>
2020-01-05 18:29:34 +0700

    Updates

---
+++ precache-manifest.ff4edaab94d679cc046c28c6bbe8b9df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "636b38862f4cad6830e0dcb7874e2d51",
+    "url": "/heikamu/index.html"
+  },
+  {
+    "revision": "76177a6c47b10c39d1c7",
+    "url": "/heikamu/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "224d81773ef6902f4670",
+    "url": "/heikamu/static/css/main.2b84145c.chunk.css"
+  },
+  {
+    "revision": "76177a6c47b10c39d1c7",
+    "url": "/heikamu/static/js/2.e165dd55.chunk.js"
+  },
+  {
+    "revision": "c980e8967e3bb70c46d132e8f0ff8b71",
+    "url": "/heikamu/static/js/2.e165dd55.chunk.js.LICENSE"
+  },
+  {
+    "revision": "224d81773ef6902f4670",
+    "url": "/heikamu/static/js/main.33a7419e.chunk.js"
+  },
+  {
+    "revision": "3518970154806fb65d78",
+    "url": "/heikamu/static/js/runtime-main.5db933c9.js"
+  }
+]);
